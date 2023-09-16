53197618b7baa37b1ebc562aa8a3b21b03e55282
Seung-9 <ssg9505fj22@naver.com>
2023-09-16 20:40:38 +0900

    feat: 상품 도메인 개발

--- src/main/java/hello/itemService/domain/Item.java
+++ src/main/java/hello/itemService/domain/Item.java
@@ -1,2 +1,17 @@
-package hello.itemService.domain;public class Item {
+package hello.itemService.domain;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
